187371bd1b03f17b909bf7a316708928846ece34
swatch <31496388+nick308@users.noreply.github.com>
2019-04-05 15:52:36 -0500

    Add files via upload

--- chat_client.cpp
+++ chat_client.cpp
@@ -99,6 +99,107 @@ public:
           mvprintw(row, i, " ");
       }
   }
+  void readSystemLog()
+  {
+      //get size of current event
+      int logLength = sysLog.size();
+
+      //loop through all events
+      for (int i = 0; i<logLength;i++)
+      {
+
+      //move into local variable
+      string str(sysLog.at(i));
+
+      //chosen format
+      string delimiter = ";";
+      //get first token
+      string action = str.substr(0, str.find(delimiter));
+      //erase first token
+      str.erase(0, str.find(delimiter)+1);
+
+      //see what type of action it is
+      if (action == "ChatMsg")
+      {
+          //get second token
+          string roomName = str.substr(0, str.find(delimiter));
+          //erase second token
+          str.erase(0, str.find(delimiter)+1);
+
+          //is user in the same room as the msg?
+          if (getRoom() == roomName)
+          {
+              //convert to char[]
+              int n = sysLogLength[i] - action.length() - roomName.length();
+              char char_array[n + 1];
+              strcpy(char_array, str.c_str());
+
+              //if it is just a regular chat message, then use this to print to screen
+              mvprintw(row,1,"%s,%i",char_array,n);
+
+
+              //cleanup UI after the 32bit pointer
+              for (int j = n-1; j<maxcol;j++)
+              {
+                  mvprintw(row,j," ");
+              }
+
+              //increment down to next line
+              row = row + 1;
+
+
+
+
+
+          }
+      }
+      //redraw the prompt at bottom left (ex- "Nick: ")
+      prompt();
+      }
+  }
+  void clearChat()
+  {
+      //wipes entire chat middle section. leaving the header and footer alone.
+      row = 2;
+      col = 1;
+      for (int r = 2;r<maxrow-2;r++)
+      {
+          for (int c = 0;c<maxcol;c++)
+          {
+              mvprintw(r, c, " ");
+          }
+      }
+  }
+  void showHelpMenu(bool show)
+  {
+
+      if (show == true)
+      {
+          clearChat();
+          string help[6];
+          help[5] = "**HELP MENU**";
+          help[4] = "/join <string> - join an existing chat room";
+          help[3] = "/leave - switch to lobby";
+          help[2] = "/create <string> - creates a new chat room";
+          help[1] = "/private <int> <string> - sends a private message";
+          help[0] = "/rename <string> - changes your account name";
+          for (int i = 0 ;i<6;i++)
+          {
+              int n = help[i].length();
+              char char_array[n + 1];
+              strcpy(char_array, help[i].c_str());
+              mvprintw(maxrow-(4+i),0, char_array);
+          }
+          prompt();
+          //read user input
+          char str[100];
+          getstr(str);
+
+      }
+
+
+  }
+
   //function redraws the header (clock, chat room name, etc) and the footer (the row of "=")
   void showHeaderFooter()
   {
@@ -127,7 +228,7 @@ public:
           //draw bottom line
       for (int i = 0;i<maxcol;i++)
       {
-          mvprintw(maxrow-3, i, "=");
+          mvprintw(maxrow-2, i, "=");
       }
   }
 private:
@@ -138,6 +239,7 @@ private:
     changeRoom("Lobby");
         //get screen size and initialize counters
     getScreen();
+
 
     asio::async_connect(socket_, endpoints,
         [this](std::error_code ec, tcp::endpoint)
@@ -175,71 +277,13 @@ private:
               {
                 if (!ec)
                 {
-                    //convert single msg to char[]
-                    string str(read_msg_.body());
+                    //store event
+                    //string str(read_msg_.body());
+                    sysLog.push_back(read_msg_.body());
+                    sysLogLength.push_back(read_msg_.body_length());
 
 
 
-                    //RoomDelete;RoomName
-                    //RoomCreate;RoomName (check for dupes)
-                    //Rename;Nick;Bob
-                    //DeleteRoom;RoomABC
-                    //ClientConnected;Nick
-                    //ChatMsg;Gaming;Nick;Hello everyone
-                    //ClientConnected;Bob
-                    //ChatMsg;Lobby;Bob; Hello from Bob
-                    //Ignore;Bob;Nick
-                    //PrivateMessage;Bob;4;Here is the message
-                    //PrivateReadInt;Nick;4
-                    //ClientDisconnected;Bob
-                    //ChatMsg;Gaming;Nick;Hello everyon
-
-
-                    //TODO - parse concat (string) or char_array (char[])
-                    //use standards that we defined
-
-                    string delimiter = ";";
-                    //get first token
-                    string action = str.substr(0, str.find(delimiter));
-                    //erase first token
-                    str.erase(0, str.find(delimiter)+1);
-
-                    //see what type of action it is
-                    if (action == "ChatMsg")
-                    {
-                        //get second token
-                        string roomName = str.substr(0, str.find(delimiter));
-                        //erase second token
-                        str.erase(0, str.find(delimiter)+1);
-
-                        //is user in the same room as the msg?
-                        if (getRoom() == roomName)
-                        {
-                            //convert to char[]
-                            int n = str.length();
-                            char char_array[n + 1];
-                            strcpy(char_array, str.c_str());
-
-                            //if it is just a regular chat message, then use this to print to screen
-                            mvprintw(row,1,"%s",char_array);
-
-                            //cleanup UI after the 32bit pointer
-                            for (int i = n+2; i<maxcol;i++)
-                            {
-                                mvprintw(row,i," ");
-                            }
-
-                            //increment down to next line
-                            row = row + 1;
-                        }
-
-
-
-                    }
-
-
-                    //redraw the prompt at bottom left (ex- "Nick: ")
-                    prompt();
                     //recursion
                     do_read_header();
                 }
@@ -280,6 +324,9 @@ private:
   int row,col;
   chat_message read_msg_;
   chat_message_queue write_msgs_;
+  vector<string> sysLog;
+  vector<int> sysLogLength;
+
 };
 
 int main(int argc, char* argv[])
@@ -347,8 +394,12 @@ int main(int argc, char* argv[])
         while (loop == 1)
         {
             //redraw top and bottom panels and prompt
+            c.clearChat();
             c.showHeaderFooter();
+            c.readSystemLog();
             c.prompt();
+
+
 
 
 
@@ -375,7 +426,7 @@ int main(int argc, char* argv[])
             //display help menu
             else if (line.find("/help") == 0)
             {
-
+                c.showHelpMenu(true);
             }
             //ignore other user
             else if (line.find("/ignore") == 0)
@@ -434,7 +485,7 @@ int main(int argc, char* argv[])
         endwin();
 
         //sunil - if user typed /quit then send disconnect msg to server
-        ///sunil - also check for ctl+c using signal handlers
+        //sunil - also check for ctl+c using signal handlers
         return 0;
 }
 
